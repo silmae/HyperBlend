6b94dfd7361c369c13126c2f7689b564ac8b7a02
kiauriih <kimmo.a.riihiaho@jyu.fi>
2021-05-15 17:44:46 +0300

    Target plotting

--- src/plotter.py
+++ src/plotter.py
@@ -10,17 +10,68 @@ from src import data_utils as DU
 from src.render_parameters import RenderParametersForSeries
 from src import toml_handlling as T
 
+figsize = (12,8)
+
+
+def plot_list_variable_to_axis(axis_object, label: str, data, skip_first=False):
+    length = len(data)
+    if skip_first:
+        axis_object.plot(np.arange(length - 1), data[1:length], label=label)
+    else:
+        axis_object.plot(np.arange(length), data, label=label)
+
+
+def plot_x_line_to_axis(axis_object, label: str, data: float, x_values, invert=False):
+    if invert:
+        axis_object.plot(x_values, 1 - np.ones((len(x_values))) * data, label=label, color='red')
+    else:
+        axis_object.plot(x_values, np.ones((len(x_values))) * data, label=label, color='red')
+
+
+def plot_refl_tran_to_axis(axis_object, refl, tran, x_values, x_label, invert_tran=False, skip_first=False):
+    refl_color = 'blue'
+    tran_color = 'orange'
+    axis_object.set_xlabel(x_label)
+    axis_object.set_ylabel('Reflectance', color=refl_color)
+    axis_object.tick_params(axis='y', labelcolor=refl_color)
+    # But use given x_values for plotting
+    length = len(x_values)
+    if skip_first:
+        axis_object.plot(x_values[1:length], refl[1:length], label="Reflectance", color=refl_color)
+    else:
+        axis_object.plot(x_values, refl, label="Reflectance", color=refl_color)
+
+    axt = axis_object.twinx()
+    axt.set_ylabel('Transmittance', color=tran_color)
+    axt.tick_params(axis='y', labelcolor=tran_color)
+    if skip_first:
+        axt.plot(x_values[1:length], tran[1:length], label="Transmittance", color=tran_color)
+    else:
+        axt.plot(x_values, tran, label="Transmittance", color=tran_color)
+
+    axis_object.set_ylim([0, 1])
+    if invert_tran:
+        axt.set_ylim([1, 0])
+    else:
+        axt.set_ylim([0, 1])
+
+
 def plot_subresult_opt_history(set_name: str, wl: float):
+
     subres_dict = T.read_subresult(set_name=set_name, wl=wl)
-    fig, ax = plt.subplots(nrows=1, ncols=2)
-    ax[0].plot(np.arange(len(subres_dict['history_absorption_density'])),subres_dict['history_absorption_density'], label='history_absorption_density')
-    ax[0].plot(np.arange(len(subres_dict['history_scattering_density'])),subres_dict['history_scattering_density'], label='history_scattering_density')
-    ax[0].plot(np.arange(len(subres_dict['history_scattering_anisotropy'])),subres_dict['history_scattering_anisotropy'], label='history_scattering_anisotropy')
-    ax[0].plot(np.arange(len(subres_dict['history_mix_factor'])),subres_dict['history_mix_factor'], label='history_mix_factor')
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=figsize)
+    plot_list_variable_to_axis(ax[0], 'history_absorption_density', subres_dict['history_absorption_density'], skip_first=True)
+    plot_list_variable_to_axis(ax[0], 'history_scattering_density', subres_dict['history_scattering_density'], skip_first=True)
+    plot_list_variable_to_axis(ax[0], 'history_scattering_anisotropy', subres_dict['history_scattering_anisotropy'], skip_first=True)
+    plot_list_variable_to_axis(ax[0], 'history_mix_factor', subres_dict['history_mix_factor'], skip_first=True)
+    ax[0].set_xlabel('iteration')
     ax[0].legend()
-    ax[1].plot(np.arange(len(subres_dict['history_reflectance'])),subres_dict['history_reflectance'], label='history_reflectance')
-    ax[1].plot(np.arange(len(subres_dict['history_transmittance'])),subres_dict['history_transmittance'], label='history_transmittance')
-    ax[1].legend()
+    plot_refl_tran_to_axis(ax[1], subres_dict['history_reflectance'], subres_dict['history_transmittance'],
+                           np.arange(len(subres_dict['history_scattering_anisotropy'])), 'iteration', invert_tran=True,
+                           skip_first=True)
+    plot_x_line_to_axis(ax[1], 'reflectance_target', subres_dict['reflectance_measured'], np.arange(1,len(subres_dict['history_reflectance'])))
+    plot_x_line_to_axis(ax[1], 'transmittance_target', subres_dict['transmittance_measured'], np.arange(1,len(subres_dict['history_transmittance'])), invert=True)
+    ax[1].set_ylim(0,1)
 
     plt.show()
 
@@ -60,27 +111,27 @@ class Plotter:
         return d
 
     def plot_wl_series(self, invert_tran_y=True, save_thumbnail=True, silent=False):
-        refl_color = 'blue'
-        tran_color = 'orange'
 
-        fig, axr = plt.subplots()
 
-        axr.set_xlabel(self.x_label)
-        axr.set_ylabel('Reflectance', color=refl_color)
-        axr.tick_params(axis='y', labelcolor=refl_color)
-        # But use given x_values for plotting
-        axr.plot(self.x_values, self.r, label="Reflectance", color=refl_color)
+        fig, axr = plt.subplots(figsize=figsize)
+        plot_refl_tran_to_axis(axr, self.r, self.t, self.x_values, self.x_label, invert_tran=True)
 
-        axt = axr.twinx()
-        axt.set_ylabel('Transmittance', color=tran_color)
-        axt.tick_params(axis='y', labelcolor=tran_color)
-        axt.plot(self.x_values, self.t, label="Transmittance", color=tran_color)
+        # axr.set_xlabel(self.x_label)
+        # axr.set_ylabel('Reflectance', color=refl_color)
+        # axr.tick_params(axis='y', labelcolor=refl_color)
+        # # But use given x_values for plotting
+        # axr.plot(self.x_values, self.r, label="Reflectance", color=refl_color)
+        #
+        # axt = axr.twinx()
+        # axt.set_ylabel('Transmittance', color=tran_color)
+        # axt.tick_params(axis='y', labelcolor=tran_color)
+        # axt.plot(self.x_values, self.t, label="Transmittance", color=tran_color)
 
-        axr.set_ylim([0, 1])
-        if invert_tran_y:
-            axt.set_ylim([1, 0])
-        else:
-            axt.set_ylim([0, 1])
+        # axr.set_ylim([0, 1])
+        # if invert_tran_y:
+        #     axt.set_ylim([1, 0])
+        # else:
+        #     axt.set_ylim([0, 1])
         # plt.legend()
 
         if save_thumbnail:
@@ -91,3 +142,5 @@ class Plotter:
 
         if not silent:
             plt.show()
+
+
