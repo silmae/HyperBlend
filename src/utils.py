9bf753b14e28805a63a573d4abc5611d7bf56de4
Kimmo Riihiaho <kimmo.a.riihiaho@jyu.fi>
2021-08-30 12:26:06 +0300

    Split list into chunks

--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,16 @@ def parse_wl_from_image_name(image_name):
     tail = image_name.split("_wl", 1)[1]
     wl_s = tail.rsplit(".", 1)[0]
     return float(wl_s)
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst.
+
+    Example
+    -----
+        original_list = [0,1,2,3,4,5,6,7,8,9]
+        chunked_list = list(chunks(original_list, 3))
+        -> [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+    """
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
