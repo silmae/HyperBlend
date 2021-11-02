c751e461f1a814e5b5c71085e975a3817bda7cfb
Kimmo Riihiaho <kimmo.a.riihiaho@jyu.fi>
2021-11-02 13:14:09 +0200

    Added real data example

--- src/__main__.py
+++ src/__main__.py
@@ -11,6 +11,8 @@ import sys
 
 # A bunch of improrts to self-made files you may need.
 from src import presets
+from src.optimization import Optimization
+from data import toml_handlling as TH
 
 if __name__ == '__main__':
     # log to stdout instead of stderr for nice coloring
@@ -18,3 +20,10 @@ if __name__ == '__main__':
 
     # Test the software with hard coded data.
     presets.optimize_default_target(spectral_resolution=50)
+
+    # Example using "real" data
+    data = [[400, 0.21435, 0.26547], [401, 0.21431, 0.26540]]
+    set_name = 'test_set'
+    o = Optimization(set_name)
+    TH.write_target(set_name, data, sample_id=0)
+    o.run_optimization()
