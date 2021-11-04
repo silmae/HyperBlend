06b753337eaba6841fd89cc8877e51978ede5efb
Kimmo Riihiaho <kimmo.a.riihiaho@jyu.fi>
2021-11-04 10:42:44 +0200

    Cleanup and documentation

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,3 +1,7 @@
+"""
+Reading and manipulating image data.
+"""
+
 import os
 
 import numpy as np
@@ -7,17 +11,18 @@ from src.data import file_handling as FH
 from src import constants as C
 
 
-def get_relative_refl_or_tran_series(imaging_type: str, wl_list, base_path: str):
-    res = []
-    for _,wl in enumerate(wl_list):
-        val = np.mean(plt.imread(FH.search_by_wl(C.target_type_leaf, imaging_type, wl, base_path)))
-        ref = np.mean(plt.imread(FH.search_by_wl(C.target_type_ref, imaging_type, wl, base_path)))
-        res.append(val / ref)
-    return np.array(res)
+def get_relative_refl_or_tran(imaging_type: str, wl: float, base_path: str) -> float:
+    """Returns leaf reflectance (or transmittance) divided by reference reflectance (or transmittance).
 
-
-def get_relative_refl_or_tran(imaging_type: str, wl: float, base_path: str):
-    """Returns leaf reflectance (transmittance) divided by reference reflectance (transmittance). """
+    :param imaging_type:
+        String, either 'refl' for reflectance or 'tran' for transmittance.
+    :param wl:
+        Wavelength.
+    :param base_path:
+        Top level path (working temp directory).
+    :return:
+        Relative reflectance or transmittance as a single float.
+    """
 
     leaf_mean = get_rend_as_mean(FH.search_by_wl(C.target_type_leaf, imaging_type, wl, base_path))
     reference_mean = get_rend_as_mean(FH.search_by_wl(C.target_type_ref, imaging_type, wl, base_path))
@@ -25,12 +30,16 @@ def get_relative_refl_or_tran(imaging_type: str, wl: float, base_path: str):
     return relative
 
 
-def get_rend_as_mean(image_file_path: os.path) -> float:
+def get_rend_as_mean(image_file_path: str) -> float:
+    """Read image from given path and return mean of the pixels."""
+
     array = get_rend_as_ndarray_wl(image_file_path)
     return np.mean(array)
 
 
-def get_rend_as_ndarray_wl(image_file_path: os.path):
+def get_rend_as_ndarray_wl(image_file_path: str):
+    """ Read an image in given path into a Numpy array."""
+
     if os.path.exists(image_file_path):
         array = plt.imread(image_file_path)
         return array
