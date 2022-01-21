76c7a95872ade2561d5aef8f3108bda081eabc28
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2022-01-21 17:14:17 +0200

    Test stuff

--- src/__main__.py
+++ src/__main__.py
@@ -17,12 +17,32 @@ if __name__ == '__main__':
     # log to stdout instead of stderr for nice coloring
     logging.basicConfig(stream=sys.stdout, level='INFO')
 
-    # Test the software with hard coded data.
-    presets.optimize_default_target(spectral_resolution=50)
+    import prospect_d as PD
+    import plotter as P
+    import matplotlib.pyplot as plt
 
-    # Example using "real" data
-    data = [[400, 0.21435, 0.26547], [401, 0.21431, 0.26540]]
-    set_name = 'test_set'
-    o = Optimization(set_name)
-    TH.write_target(set_name, data, sample_id=0)
-    o.run_optimization()
+    l, r, t = PD.run_prospect(
+    n = 1.5,
+    cab = 32,
+    car = 8,
+    cbrown = 0.,
+    cw = 0.016,
+    cm = 0.009,
+    ant = 0.0,
+    nr = None, kab = None, kcar = None, kbrown = None, kw = None,
+    km = None, kant = None, alpha = 40.)
+    print('moi')
+
+    fig, ax = plt.subplots()
+    P._plot_refl_tran_to_axis(ax, r, t, l, x_label='wavelength', invert_tran=True)
+    plt.show()
+
+    # # Test the software with hard coded data.
+    # presets.optimize_default_target(spectral_resolution=50)
+    #
+    # # Example using "real" data
+    # data = [[400, 0.21435, 0.26547], [401, 0.21431, 0.26540]]
+    # set_name = 'test_set'
+    # o = Optimization(set_name)
+    # TH.write_target(set_name, data, sample_id=0)
+    # o.run_optimization()
