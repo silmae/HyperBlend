780d48aae2e5bc454d39c1ccf3d0dc075e519139
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2022-08-29 11:52:07 +0300

    Cleaned imports

--- src/leaf_model/interface.py
+++ src/leaf_model/interface.py
@@ -10,8 +10,6 @@ import time
 import logging
 
 import src.leaf_model.training_data
-from src.leaf_model.leaf_commons import  _convert_raw_params_to_renderable, \
-    _material_params_to_RT, _build_sample_res_dict, initialize_directories
 from src.leaf_model.opt import Optimization
 from src.data import file_handling as FH, toml_handling as TH
 from src import plotter
@@ -26,11 +24,10 @@ def solve_leaf_material_parameters(set_name: str, resolution=1, solver='nn', cle
         logging.info(f"Prospect parameter generation asked but I don't know how yet.")
         # TODO generate new targets and save to disk with given name
 
-    # TODO copyof some other measurement set
     if copyof:
         FH.copy_target(from_set=copyof, to_set=set_name)
     else:
-        initialize_directories(set_name=set_name, clear_old_results=clear_old_results)
+        LC.initialize_directories(set_name=set_name, clear_old_results=clear_old_results)
 
     ids = FH.list_target_ids(set_name)
     ids.sort()
@@ -65,15 +62,15 @@ def solve_leaf_material_parameters(set_name: str, resolution=1, solver='nn', cle
                 else:
                     ad_raw, sd_raw, ai_raw, mf_raw = nn.predict(r_m=r_m, t_m=t_m)
 
-            ad, sd, ai, mf = _convert_raw_params_to_renderable(ad_raw, sd_raw, ai_raw, mf_raw)
-            r, t = _material_params_to_RT(set_name, sample_id, wls, ad, sd, ai, mf)
+            ad, sd, ai, mf = LC._convert_raw_params_to_renderable(ad_raw, sd_raw, ai_raw, mf_raw)
+            r, t = LC._material_params_to_RT(set_name, sample_id, wls, ad, sd, ai, mf)
 
             re = np.abs(r - r_m)
             te = np.abs(t - t_m)
             running_time = (time.perf_counter() - start) / 60.0
             time_process_min = running_time
             time_wall_clock_min = running_time
-            sample_result_dict = _build_sample_res_dict(wls, r, r_m, re, t, t_m, te, ad_raw, sd_raw, ai_raw, mf_raw, time_process_min, time_wall_clock_min)
+            sample_result_dict = LC._build_sample_res_dict(wls, r, r_m, re, t, t_m, te, ad_raw, sd_raw, ai_raw, mf_raw, time_process_min, time_wall_clock_min)
 
             TH.write_sample_result(set_name, sample_result_dict, sample_id)
 
