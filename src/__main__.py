cdf62dfd1c6416bcbdea93bca182423769f06edf
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2022-11-28 17:04:31 +0200

    Added usage examples to main

--- src/__main__.py
+++ src/__main__.py
@@ -2,7 +2,7 @@
 Entry point of the program.
 
 There is no user interface to the program (yet) so code your calls here
-and run in IDE.
+and run in your favourite IDE.
 """
 
 import logging
@@ -18,11 +18,8 @@ from src.leaf_model import surf as SM
 from src.data import path_handling as PH
 
 from src.data import toml_handling as TH
-from src.leaf_model import interface
+from src.leaf_model import interface as LI
 from src import constants as C
-
-
-
 
 if __name__ == '__main__':
     # log to stdout instead of stderr for nice coloring
@@ -43,3 +40,40 @@ if __name__ == '__main__':
                             logging.FileHandler(log_path, mode='w'),
                             logging.StreamHandler()
                         ])
+
+    # Let's first generate some random PROSPECT leaves
+    set_name = "try_random_p_leaves"
+    # generates three leaf targets to \HyperBlend\leaf_measurement_sets\try_random_p_leaves\sample_targets
+    LI.generate_prospect_leaf_random(set_name=set_name, count=3)
+    # Solve renderable leaf material parameters that produce target reflectance and transmittance
+    LI.solve_leaf_material_parameters(set_name=set_name, resolution=10, solver='nn')
+    # After solver has run, check results from HyperBlend\leaf_measurement_sets\try_random_p_leaves\set_result
+
+
+    # Similarly, we can provide exact parameters
+    set_name = "try_p_leaves"
+    # generates a leaf target with certain parameters to \HyperBlend\leaf_measurement_sets\try_p_leaves\sample_targets.
+    # The values used here are the default values.
+    LI.generate_prospect_leaf(set_name=set_name, sample_id=0, n=1.5, ab=32, ar=8, brown=0, w=0.016, m=0.009, ant=0)
+    # You can also give only some parameters. Defaults will be used for the ones not provided.
+    # Remember to give new sample_id so that the previously created leaf is not overwritten.
+    LI.generate_prospect_leaf(set_name=set_name, sample_id=1, w=0.001, m=0.03)
+    # Solve renderable leaf material parameters as before
+    LI.solve_leaf_material_parameters(set_name=set_name, resolution=10, solver='nn')
+    # After solver has run, check results from HyperBlend\leaf_measurement_sets\try_p_leaves\set_result
+
+
+    # We can also copy existing set and solve it with a different solver for example. Let's try that with
+    #   surface fitting solver called 'surf'
+    copy_set = "try_copying_set"
+    LI.solve_leaf_material_parameters(set_name=copy_set, resolution=10, solver='surf', copyof="try_p_leaves")
+
+
+    # Let's try manually creating some data to work with
+    set_name = "try_manual_set"
+    # Example data list of lists where inner list holds the data ordered as [wavelength, reflectance, transmittance]
+    data = [[400, 0.21435, 0.26547], [401, 0.21431, 0.26540]]
+    # Write data to disk in a format the HyperBlend can understand
+    TH.write_target(set_name, data, sample_id=0)
+    # Solve as before
+    LI.solve_leaf_material_parameters(set_name=set_name, resolution=10, solver='nn')
