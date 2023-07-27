e439687105116bdca08041e133882574606cf84c
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2023-07-27 14:16:10 +0300

    Proper ENVI header. Improved cube show

--- src/data/cube_handling.py
+++ src/data/cube_handling.py
@@ -3,12 +3,28 @@ import numpy as np
 import spectral
 import os
 import matplotlib.pyplot as plt
+import logging
+import csv
 
 from src.data import path_handling as PH
 from src.data import file_handling as FH
+from src.utils import data_utils as DU, spectra_utils as SU
+from src import constants as C
 
 
-def rended_to_envi_cube(forest_id: str):
+def construct_envi_cube(forest_id: str):
+    """Constructs an ENVI style hyperspectral image cube out of rendered images.
+
+    Can be used after the scene has been rendered (at least spectral and visibility maps).
+
+    White reference for reflectance calculation is searched automatically from
+    available visibility maps. Note that the maps must be named like "Reference 0.00 material...".
+
+    Default RGB bands for ENVI metadata are inferred if in visible range.
+    Otherwise first, middle, and last bands are used.
+
+    :return:
+    """
 
     p = PH.path_directory_forest_rend_spectral(forest_id=forest_id)
     if not os.path.exists(p):
@@ -26,18 +42,81 @@ def rended_to_envi_cube(forest_id: str):
         print(thing)
 
     raw_cube = np.array(frame_list)
-    # TODO below hack radiance
-    reflectance_cube = np.divide(raw_cube, raw_cube.max(), dtype=np.float32)
+
+    # Burnt areas have values around 65535
+    # Loop white references until the image is not burned
+    max_burn = 65000.0
+    white_mean = max_burn
+
+    # Find available reflectance plate reflectivity based on visibility map file names.
+    reflectivities = []
+    map_names = PH.list_reference_visibility_maps(forest_id=forest_id)
+    for map_name in map_names:
+        splitted = map_name.split(' ')
+        reflectivity = float(splitted[1])
+        if reflectivity > 0.0:
+            reflectivities.append(reflectivity)
+
+    reflectivities.sort(reverse=True)
+
+    logging.info(f"Searching for good white reference plate..")
+    accepted_reflectivity = None
+    for reflectivity in reflectivities:
+        accepted_reflectivity = reflectivity
+        mask_path = PH.find_reference_visibility_map(forest_id=forest_id, reflectivity=reflectivity)
+        mask = plt.imread(mask_path)
+        mask = mask > 0
+        white_cube = raw_cube[:,mask] # Flattens the reference plate area pixels..
+        white_mean = np.mean(white_cube, axis=(1)) #.. so we take the mean only on one axis.
+        white_mean_max = white_mean.max()
+        if white_mean_max < max_burn:
+            break
+
+    logging.info(f"Accepted white reference with {accepted_reflectivity:.2f} reflectivity producing maximum mean reflectance {white_mean_max:.1f}.")
+
+    white_mean = np.expand_dims(white_mean, axis=(1,2))
+    reflectance_cube = np.divide(raw_cube, white_mean, dtype=np.float32)
+    # refl_max = np.max(reflectance_cube)
+
+    # Swap axis to arrange the array as expected by spectral.envi
     reflectance_cube = np.swapaxes(reflectance_cube, 0,2)
     reflectance_cube = np.swapaxes(reflectance_cube, 0,1)
-    print(f"new shape: {reflectance_cube.shape}")
+
+    p = PH.path_file_forest_sun_csv(forest_id=forest_id)
+    if not os.path.exists(p):
+        logging.warning(f"Could not find sun data for wavelength info. The image cube will be saved without it.")
+
+    # Retrieve band and wavelength info from the sun file.
+    wls = []
+    bands = []
+    with open(p) as file:
+        reader = csv.reader(file, delimiter=' ')
+        for row in reader:
+
+            if "band" in row:
+                continue # header row
+
+            bands.append(int(row[0]))
+            wls.append(float(row[1]))
+
+    # Define default RGB bands.
+    if SU.is_in_visible(wls=wls):
+        nearest_R_idx = SU.find_nearest_idx(wls, C.default_R_wl)
+        nearest_G_idx = SU.find_nearest_idx(wls, C.default_G_wl)
+        nearest_B_idx = SU.find_nearest_idx(wls, C.default_B_wl)
+        default_bands = [bands[nearest_R_idx], bands[nearest_G_idx], bands[nearest_B_idx]]
+    else:
+        default_bands = [bands[-1], bands[int(len(bands)/2)], bands[0]]
 
     header_dict = {
         "bands" : reflectance_cube.shape[0],
         "lines": reflectance_cube.shape[1],
         "samples": reflectance_cube.shape[2],
         "data_type": 4,
-        # TODO read wavelengths to metadata
+        "reference reflectivity": accepted_reflectivity,
+        "default bands": default_bands,
+        "wavelength": wls,
+        "wavelength units": "nm",
     }
 
     cube_dir_path = PH.path_directory_forest_cube(forest_id)
@@ -50,11 +129,32 @@ def rended_to_envi_cube(forest_id: str):
 
 
 def show_cube(forest_id: str):
+    """Shows the hyperspectral image cube.
 
-    # spectral.envi.open()
+    Use construct_envi_cube() to generate it.
+
+    :param forest_id:
+        Forest scene id.
+    :return:
+        None
+    :raises
+        FileNotFoundError if the cube does not exist.
+    """
+
     p_cube = PH.path_file_forest_reflectance_header(forest_id=forest_id)
+    if not os.path.exists(p_cube):
+        raise FileNotFoundError(f"Cannot find spectral cube file from '{p_cube}'. "
+                                f"Use construct_envi_cube() to generate the cube from rendered images.")
     data = spectral.open_image(p_cube)
-    rgb = data.read_bands(bands=(1,2,3))
+
+    # Minus 1 because spectral is zero-based and ENVI standard one-based.. apparently.
+    default_bands = [int(band) - 1 for band in data.metadata['default bands']]
+
+    rgb = data.read_bands(bands=default_bands)
+    plt.close('all') # Close all previous plots before showing this one.
+    plt.figure(figsize=(10,10))
     plt.imshow(rgb)
     plt.show()
-    # view = spectral.imshow(data, bands=(0,1,2))
+
+    # TODO Would be nice if this worked, but it just flashes on the screen
+    # view = spectral.imshow(data, bands=default_bands)
