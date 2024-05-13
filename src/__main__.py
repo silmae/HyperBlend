4edb1c5c8011a978178b7a591aef3afc9edc1895
Kimmo <kimmo.a.riihiaho@jyu.fi>
2024-05-13 16:59:03 +0300

    Pipetest now uses explicitly default sun and sky and precalc soil

--- src/__main__.py
+++ src/__main__.py
@@ -40,32 +40,34 @@ def read_forest_control(forest_id: str) -> dict:
 def forest_pipe_test(rng):
 
     # Generating low resolution random leaves
-    set_name = 'low_res_w_dry'
-    new_sampling = [450,500,550,600,700,800]
-    # LI.generate_prospect_leaf_random(set_name=set_name, leaf_count=2)
-    # LI.generate_prospect_leaf(set_name=set_name, sample_id=4) # add one dry default leaf
-    # LI.resample_leaf_targets(set_name=set_name, new_sampling=new_sampling)
-    # LI.solve_leaf_material_parameters(set_name=set_name, clear_old_results=True)
-    #
-    # leaves = [(set_name, 0, 'Leaf material 1'), (set_name, 1, 'Leaf material 2'), (set_name, 3, 'Leaf material 3')]
-    # forest_id = forest.init(leaves=leaves, conf_type='m2m', rng=rng, custom_forest_id='sky_test')
+    set_name = "demo_leaves"
+    new_sampling = [450,500,550,600,700,800,900,1000]
+    LI.generate_prospect_leaf_random(set_name=set_name, leaf_count=2) # generate 2 random leaf spectra
+    LI.generate_prospect_leaf(set_name=set_name, sample_id=3, w=0.001) # add one dry default leaf spectra
+    LI.resample_leaf_targets(set_name=set_name, new_sampling=new_sampling) # resample leaf spectra
+    LI.solve_leaf_material_parameters(set_name=set_name, clear_old_results=True) # run slab simulation
 
-    """
-    Running forest.init only copies files. Running setup makes the Blender scene renderable.
-    """
+    # Pack leaf data for forest scene initialization
+    leaves = [(set_name, 0, 'Leaf material 1'), (set_name, 1, 'Leaf material 2'), (set_name, 3, 'Leaf material 3')]
+    forest_id = "demo_forest"
+    # Use pre-calculated soil spectra and default sun and sky spectra. They are automatically interpolated to
+    #   match the leaf spectra bands.
+    soil_name = "median_humid_clay_reflectance"
+    sun_name = "default_sun"
+    sky_name = "default_sky"
+    forest.init(leaves=leaves, conf_type='m2s', rng=rng, custom_forest_id=forest_id, soil_name=soil_name,
+                sun_file_name=sun_name, sky_file_name=sky_name)
 
-    forest_id = 'sky_test'
-
+    # Running forest.init only copies files. Running setup makes the Blender scene renderable.
     BC.setup_forest(forest_id=forest_id, leaf_name_list=['Leaf material 1', 'Leaf material 2', 'Leaf material 3'])  #, 'Leaf material 4'])
 
-    # BC.render_forest(forest_id=forest_id, render_mode='preview')
-    # BC.render_forest(forest_id=forest_id, render_mode='visibility')
+    # Render bands for spectral cube along with additional images
+    BC.render_forest(forest_id=forest_id, render_mode='preview')
+    BC.render_forest(forest_id=forest_id, render_mode='visibility')
     BC.render_forest(forest_id=forest_id, render_mode='spectral')
 
+    # Construct spectral cube in ENVI format
     CH.construct_envi_cube(forest_id=forest_id)
-    # CH.show_cube(forest_id=forest_id)
-
-    # BC.generate_forest_control(global_master=True)
 
 
 def run_paper_tests():
