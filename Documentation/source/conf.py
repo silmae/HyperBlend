bde3fcda7b047ffbff03fe71efbb41cf51f37b90
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2026-07-20 15:28:49 +0300

    Cleanup

--- Documentation/source/conf.py
+++ Documentation/source/conf.py
@@ -12,15 +12,7 @@ import os
 import sys
 
 os.chdir("../../")
-
-#sys.path.insert(0, os.path.abspath("../../"))
 sys.path.insert(0, os.path.abspath("./"))
-
-#sys.path.append(os.path.abspath("./src/blender_scripts/"))
-
-#for x in os.walk("../../src"):
-    #sys.path.insert(1, os.path.abspath(x[0]))
-    #sys.path.append(os.path.abspath(x[0]))
 
 for x in os.walk("./src"):
     sys.path.insert(1, os.path.abspath(x[0]))
@@ -28,16 +20,15 @@ for x in os.walk("./src"):
 # -- Project info --------------------------------------------
 
 project = "HyperBlend"
-copyright = "2025, Kimmo Riihiaho"
+copyright = "2026, Kimmo Riihiaho"
 author = "Kimmo Riihiaho"
-release = "0.3"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
-    #    "myst_parser",
     "sphinx.ext.mathjax",  # for Latex math
     "sphinxcontrib.bibtex",  # bibtex style citations
 ]
@@ -58,7 +49,6 @@ add_module_names = False  # clear module names out of function signatures
 autodoc_mock_imports = [
     "bpy",  # Do not try to truly import Blender's bpy
 ]
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
