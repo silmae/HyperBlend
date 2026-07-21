b30078a2fdd8bceb74bad5d0b6d95bed03bf3351
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2026-07-21 12:32:34 +0300

    Few words about test in general

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,10 +1,15 @@
 """
+The tests are not comprehensive, but they do test that the most
+core functionality is working. The tests mostly check that correct
+files and directories are created and they do have some content.
+The actual content is not checked in most cases.
 
-Things that should be tested:
+Things that are tested:
 
     - Target generation
     - Resampling
     - Slab simulation using the default:
+
         - optimizer
         - surface fitting
         - nn
