1501bbac78c279cadf2ec4774aa2ea3cf50959ba
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2026-07-28 11:19:13 +0300

    Bundle run test in main

--- src/__main__.py
+++ src/__main__.py
@@ -22,5 +22,16 @@ if __name__ == "__main__":
 
     runtime = initialization.initialize()
 
-    system_sim_name = "My simulation"
-    F.construct_spectral_cube(system_sim_name=system_sim_name)
+    # system_sim_name = "My simulation"
+    bundle_name = "My second bundle"
+    slab_material_names = ["Slab material 1", "Slab material 2", "Slab material 3"]
+
+    # Pack leaf data for system_simulation scene initialization.
+    # leaves = [
+    #     (slab_sim_name, 0, slab_material_names[0]),
+    #     (slab_sim_name, 1, slab_material_names[1]),
+    #     (slab_sim_name, 3, slab_material_names[2]),
+    # ]
+
+    # F.create_scene_bundle(bundle_name=bundle_name, system_sim_name_ancestor=bundle_ancestor_name, rng=rng, count=3, leaves=leaves)
+    F.run_scene_bundle(runtime=runtime, bundle_name=bundle_name, slab_material_names=slab_material_names)
